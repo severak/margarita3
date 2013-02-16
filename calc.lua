c0fb434fccd328a0ec124c49ca4098975c66db5e
Spravce <Spravce>
2013-02-16 14:36:01 +0000

    Funkcni kalkulacka casu. Working time calc.

--- calc.lua
+++ calc.lua
@@ -1,9 +1,39 @@
+#!/usr/bin/env lua
 ----
 -- Time calculator
 ----
 -- part of Margarita3 project
--- (c) Sever·k 2012
+-- (c) Sever√°k 2013
 -- License: MIT
 ----
 local timelib=require "margarita.time_utils"
 
+print('Time calculator')
+
+local mem=0
+
+repeat
+	cmd=io.read()
+	if cmd:match("[%/%*]%d+") then
+		local sign, mul = cmd:match("([%/%*])(%d+)")
+		if sign=="*" then
+			mem=mem*mul
+		elseif sign=="/" then
+			mem=mem/mul
+		end
+	elseif cmd:match("[+-]-[%d]+") then
+		local sign, add = cmd:match("([+-]-)([:%d]+)")
+		local add = timelib.to_number(add)
+		if sign=="-" then
+			mem=mem-add
+		else
+			mem=mem+add
+		end
+	elseif cmd=="=" then
+		print(timelib.to_time(mem))
+	elseif cmd=="==" then
+		print(timelib.to_time(mem))
+		print(" ")
+		mem=0
+	end
+until cmd=="x"
